16017989b8e2eac64060c6f4733eedf90dcc860e
shivamgadge <128356108+shivamgadge@users.noreply.github.com>
2023-07-12 11:40:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// echo "HELLO WORLD";
+echo "HELLO WORLD";
+echo "PANI PANI ";
+$num1 = 5;
+$num2 = 6;
+$sum= $num1 + $num2;
+echo "$num1 +$num2 = $sum <br>"; 
+
+$x=5;
+$y=6;
+$x=$x+$y;
+$y=$x-$y;
+$x=$x-$y;
+echo"$x,$y";
+
+
+
+
+
+
+
+?>
